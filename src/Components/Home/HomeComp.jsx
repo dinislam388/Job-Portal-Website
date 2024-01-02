e45c43f8a35608f6c6f461819d448269ae3af794
dinislam388 <dinislam01319950749@gmail.com>
2024-01-02 10:43:23 +0600

    added explore details

--- src/Components/Home/HomeComp.jsx
+++ src/Components/Home/HomeComp.jsx
@@ -105,10 +105,9 @@ const HomeComp = () => {
                       </radialGradient>
                     </defs>
                   </svg>
-                  <p className="card-title">Zero-cost Possibilities</p>
+                  <p className="card-title">Fresher Developer</p>
                   <p className="card-description">
-                    Hubble lets users borrow USDH for a one-time 0.5% fee. No
-                    variable rates. No interest charged, ever.
+                  As a Senior Developer, you'll play a crucial role in shaping our cutting-edge projects and leading a team of talented professionals.
                   </p>
                 </div>
               </div>
@@ -209,7 +208,7 @@ const HomeComp = () => {
                       </radialGradient>
                     </defs>
                   </svg>
-                  <p className="card-title">Multi-Asset Collateral</p>
+                  <p className="card-title">Junior Developer</p>
                   <p className="card-description">
                     Deposit a variety of assets on Hubble, raise your collateral
                     ratio, and unlock the liquidity in your wallet.
@@ -279,87 +278,19 @@ const HomeComp = () => {
                       </radialGradient>
                     </defs>
                   </svg>
-                  <p className="card-title">Get Yield on Deposits</p>
+                  <p className="card-title">Senior Developer</p>
                   <p className="card-description">
-                    While your collateral is deposited, delegate it to earn the
-                    highest yield available in the Solana ecosystem.
+                  As a Senior Developer, you'll play a crucial role in shaping our cutting-edge projects and leading a team of talented professionals.
                   </p>
                 </div>
               </div>
-
-              <div className="card">
-                <div className="container-card bg-blue-box">
-                  <svg
-                    width="80"
-                    height="80"
-                    viewBox="0 0 120 120"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <rect
-                      x="1"
-                      y="1"
-                      width="118"
-                      height="118"
-                      rx="24"
-                      fill="url(#paint0_linear_1366_4582)"
-                      fill-opacity="0.15"
-                      stroke="url(#paint1_radial_1366_4582)"
-                      stroke-width="2"
-                    ></rect>
-                    <path
-                      d="M90.9405 64.775L88.0155 69.55L85.1155 64.775H80.0655L85.5155 72.85L79.5905 82H84.4905L88.0155 76.175L91.5155 82H96.4405L90.4905 72.85L95.9405 64.775H90.9405Z"
-                      fill="#87A1FF"
-                    ></path>
-                    <path
-                      d="M44.9833 35.52L27.3433 43.92V52.53L35.1833 49.17V82H44.9833V35.52ZM71.37 35.52L53.73 43.92V52.53L61.57 49.17V82H71.37V35.52Z"
-                      fill="#87A1FF"
-                    ></path>
-                    <defs>
-                      <linearGradient
-                        id="paint0_linear_1366_4582"
-                        x1="120.194"
-                        y1="119.827"
-                        x2="-13.1225"
-                        y2="17.1841"
-                        gradientUnits="userSpaceOnUse"
-                      >
-                        <stop stop-color="#61A0FF" stop-opacity="0.7"></stop>
-                        <stop
-                          offset="0.489583"
-                          stop-color="#61A0FF"
-                          stop-opacity="0"
-                        ></stop>
-                        <stop
-                          offset="1"
-                          stop-color="#61A0FF"
-                          stop-opacity="0.7"
-                        ></stop>
-                      </linearGradient>
-                      <radialGradient
-                        id="paint1_radial_1366_4582"
-                        cx="0"
-                        cy="0"
-                        r="1"
-                        gradientUnits="userSpaceOnUse"
-                        gradientTransform="translate(60 60) rotate(96.8574) scale(122.674 149.921)"
-                      >
-                        <stop stop-color="#87A1FF"></stop>
-                        <stop
-                          offset="1"
-                          stop-color="#16161D"
-                          stop-opacity="0.2"
-                        ></stop>
-                      </radialGradient>
-                    </defs>
-                  </svg>
-                  <p className="card-title">Get up to 11x Leverage</p>
-                  <p className="card-description">
-                    Hubble’s capital-efficient 110% collateral ratio lets users
-                    leverage up to 11x on their deposits.
-                  </p>
-                </div>
-              </div>
+            </div>
+          </div>
+          <div className="ebtn">
+            <div className="ebtn-b">
+              <Link to="/users">
+                <button>SEE MORE...</button>
+              </Link>
             </div>
           </div>
         </div>
--- src/Components/User/User.jsx
+++ src/Components/User/User.jsx
@@ -1,5 +1,10 @@
 import { Link } from "react-router-dom";
 import "./User.css";
+import { MdFavoriteBorder } from "react-icons/md";
+import { MdAddCircle } from "react-icons/md";
+import { FaRegEdit } from "react-icons/fa";
+import { RiDeleteBin5Line } from "react-icons/ri";
+
 const User = ({ user }) => {
   const { id, logo, companyName, position, title, description } = user;
   return (
@@ -7,13 +12,23 @@ const User = ({ user }) => {
       <div className="jobCards">
         <div className="jobLogo">
           <img src={logo} alt="" />
+            <div className="jobCrad">
+              <MdFavoriteBorder />
+              <MdAddCircle/>
+              <FaRegEdit />
+              <RiDeleteBin5Line />
+            </div>
         </div>
         <h3 className="companyName">{companyName}</h3>
         <h2 className="jobPosition">{position}</h2>
         <h4 className="jobTitle">{title}</h4>
-        <div>
+        <div className="cardBtn">
           <Link className="dtailsBtn" to={`/users/${id}`}>
             <button>See Details</button>
+          </Link>
+
+          <Link className="dtailsBtn" to={`/users/${id}`}>
+            <button>Apply Now</button>
           </Link>
         </div>
       </div>
